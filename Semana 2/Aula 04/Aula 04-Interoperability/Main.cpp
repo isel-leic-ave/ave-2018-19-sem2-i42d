5d439a641cb1c99081aba119b4bba8cdc4e2cc39
Nuno Leite <nunocsleite@gmail.com>
2019-02-25 15:54:45 +0000

    Aula 4

---
+++ Semana 2/Aula 04/Aula 04-Interoperability/Main.cpp
@@ -0,0 +1,18 @@
+
+// C++/CLI code (C++ for .NET)
+
+#using "Rectangle.dll"
+
+using namespace System;
+
+
+void main()
+{
+	Rectangle ^rect = gcnew Rectangle();
+	rect->length = 4;
+    rect->breadth = 5;
+	Console::WriteLine(rect->Area());
+	Console::WriteLine(rect->Perimeter());
+}
+
+// cl /clr:safe Main.cpp
---
+++ Semana 2/Aula 04/Aula 04-Interoperability/Rectangle.cs
@@ -0,0 +1,14 @@
+
+// C# code
+
+// Child class
+public class Rectangle : Shape
+{
+	override public float Area() 
+	{
+		return base.length * base.breadth;
+	}
+}
+
+
+// csc /addmodule:Shape.netmodule /t:library Rectangle.cs
