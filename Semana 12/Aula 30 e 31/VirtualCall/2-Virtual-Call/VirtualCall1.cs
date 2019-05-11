2a11c20ace84fb340add7202617005253913754c
Nuno Leite <nunocsleite@gmail.com>
2019-05-11 11:57:49 +0100

    Semana 12 - Aula 30 e 31

---
+++ Semana 12/Aula 30 e 31/VirtualCall/2-Virtual-Call/VirtualCall1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace Aulas2526.VirtualCall1
+{
+    class A {
+        public virtual void M1() {
+            Console.WriteLine("virtual A::M1");
+        }
+    }
+
+    class B : A
+    {
+        public void M2()
+        {
+            Console.WriteLine("B::M2");
+        }
+    }
+
+    class C : B
+    {
+        public override void M1()
+        {
+            Console.WriteLine("virtual C::M1 - override A::M1");
+        }
+        public void M3()
+        {
+            Console.WriteLine("C::M3");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main1()
+        {
+            A c = new C();
+            c.M1();  // C::M1
+            //c.M2(); // Erro, Tipo A nao contem M2
+            //((B)c).M2();
+
+            B c2 = (B)c;
+            c2.M2(); // Despacho estatico
+
+            A b = new B();
+            b.M1(); // B::M1 <=> A::M1
+        }
+    }
+}
+/*
+virtual C::M1 - override A::M1
+virtual A::M1
+
+*/ 
