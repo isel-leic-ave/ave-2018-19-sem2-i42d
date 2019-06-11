374f53c2c9705294beb3a25e605dea0596f92a84
Nuno Leite <nunocsleite@gmail.com>
2019-06-11 10:17:19 +0100

    Semana 16 - Aula 42 e 43

---
+++ Semana 16/Aula 42 e 43/EnumeradoresELinq/LINQ/CustomersWithoutLINQ.cs
@@ -0,0 +1,34 @@
+
+
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+using MyExtensions1; // To use my extension methods
+
+namespace LinqExample
+{
+    public class CustomerWithoutLinq
+    {
+
+        static void Main(string[] args)
+        {
+            // Declaration and initialization of an array of anonymous types
+            var customers = new[]{
+            new { Name = "Marco", Discount = 4.5 },
+            new { Name = "Paolo", Discount = 3.0 },
+            new { Name = "Tom",   Discount = 3.5 }
+        };
+
+            var query = customers
+                            .Where(c => c.Discount > 3)
+                            .OrderBy(c => c.Discount)
+                            .Select(c => new { c.Name, Perc = c.Discount / 100 });
+
+            foreach (var x in query)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
